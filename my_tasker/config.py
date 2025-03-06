6f6221ec166091be508f9883a1f1fe8b7316919b
Pavel <pavelvodocnikov@gmail.com>
2025-03-06 22:56:36 +0500

    Emoji, DBData

--- my_tasker/config.py
+++ my_tasker/config.py
@@ -1,10 +1,25 @@
+from aiogram import Em
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class BotConfig(BaseSettings):
     bot_token: SecretStr
+    db_name: SecretStr
+    db_user: SecretStr
+    db_host: SecretStr
+    db_password: SecretStr
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 bot_config = BotConfig()
+
+
+class Emoji:
+    """Эмодзи"""
+
+    OK = "✅"
+    NO = "❌"
+    ASK = "❓"
+    WARNING = "⚠️"
+    ERROR = "⛔️"
