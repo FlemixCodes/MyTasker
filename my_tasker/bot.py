52334d34a33d53c35ecb0597e4cb41919049c363
Pavel <pavelvodocnikov@gmail.com>
2025-03-06 23:03:49 +0500

    логирование

--- my_tasker/bot.py
+++ my_tasker/bot.py
@@ -1,5 +1,7 @@
+import coloredlogs
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiogram.loggers import dispatcher, event, middlewares, scene, webhook
 
 from my_tasker.config import bot_config
 from my_tasker.handlers import routers_list
@@ -7,6 +9,13 @@ from my_tasker.handlers import routers_list
 
 async def start_bot():
     """Запустить бота"""
+    log_fmt = "%(asctime)s | %(levelname)s %(name)s | - %(message)s"
+    coloredlogs.install(level="DEBUG", logger=dispatcher, fmt=log_fmt)
+    coloredlogs.install(level="DEBUG", logger=event, fmt=log_fmt)
+    coloredlogs.install(level="DEBUG", logger=middlewares, fmt=log_fmt)
+    coloredlogs.install(level="DEBUG", logger=scene, fmt=log_fmt)
+    coloredlogs.install(level="DEBUG", logger=webhook, fmt=log_fmt)
+
     bot = Bot(
         token=bot_config.bot_token.get_secret_value(),
         default=DefaultBotProperties(parse_mode="HTML"),
