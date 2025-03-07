7259b3b4a285b93a87e4f12c08f807a46b7d661a
Pavel <pavelvodocnikov@gmail.com>
2025-03-07 20:27:23 +0500

    tasks data

--- my_tasker/handlers/tasks.py
+++ my_tasker/handlers/tasks.py
@@ -1,4 +1,4 @@
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
@@ -6,16 +6,20 @@ router = Router(name="tasks")
 
 
 @router.message(Command("tasks"))
+@router.message(F.data == "get_tasks")
 async def get_tasks(message: Message): ...
 
 
 @router.message(Command("task"))
+@router.message(F.data == "get_task")
 async def get_task(message: Message): ...
 
 
 @router.message(Command("add_task"))
+@router.message(F.data == "add_task")
 async def add_task(message: Message): ...
 
 
 @router.message(Command("delete_task"))
+@router.message(F.data == "delete_task")
 async def delete_task(message: Message): ...
