cbb27843b9095316153ef6588b0e202a381ef84c
Pavel <pavelvodocnikov@gmail.com>
2025-03-07 20:34:08 +0500

    update

--- my_tasker/handlers/basic.py
+++ my_tasker/handlers/basic.py
@@ -5,8 +5,10 @@ from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 
 from my_tasker.config import Emoji, bot_config
+from my_tasker.keyboards.menu import menu_keyboard
 
 router = Router(name="basic")
+
 
 @router.message(CommandStart())
 async def start_handler(message: Message):
@@ -19,6 +21,12 @@ async def ping_handler(message: Message):
     latency = round(datetime.now().timestamp() - message.date.timestamp(), 2) * 1000
     await message.answer(f"{Emoji.SETTINGS} Бот ответил за: <b>{latency}</b>мс.")
 
+
 @router.message(Command("privacy"))
 async def privacy_handler(message: Message):
     await message.answer(f"{bot_config.privacy.get_secret_value()}")
+
+
+@router.message(Command("menu"))
+async def get_menu(message: Message):
+    await message.answer("Выберите действие:", reply_markup=menu_keyboard.as_markup())
--- my_tasker/keyboards/menu.py
+++ my_tasker/keyboards/menu.py
@@ -2,9 +2,15 @@ from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 menu_keyboard = InlineKeyboardBuilder()
-menu_keyboard.add(InlineKeyboardButton("Добавить задачу", callback_data="add_task"))
-menu_keyboard.add(InlineKeyboardButton("Получить задачу", callback_data="get_task"))
-menu_keyboard.add(InlineKeyboardButton("Удалить задачу", callback_data="delete_task"))
 menu_keyboard.add(
-    InlineKeyboardButton("Получить список задач", callback_data="get_tasks")
+    InlineKeyboardButton(text="Добавить задачу", callback_data="add_task")
+)
+menu_keyboard.add(
+    InlineKeyboardButton(text="Получить задачу", callback_data="get_task")
+)
+menu_keyboard.add(
+    InlineKeyboardButton(text="Удалить задачу", callback_data="delete_task")
+)
+menu_keyboard.add(
+    InlineKeyboardButton(text="Получить список задач", callback_data="get_tasks")
 )
