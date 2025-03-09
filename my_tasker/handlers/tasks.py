f747b06f56ccca8888d1d888ee1e4c535bbd3a41
Pavel <pavelvodocnikov@gmail.com>
2025-03-09 12:01:24 +0500

    fix

--- my_tasker/handlers/tasks.py
+++ my_tasker/handlers/tasks.py
@@ -2,7 +2,7 @@ from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
-from my_tasker.database.database import database
+from my_tasker.database import client
 from my_tasker.utils.lang import get_string
 
 router = Router(name="tasks")
@@ -11,7 +11,6 @@ router = Router(name="tasks")
 @router.message(Command("tasks"))
 @router.message(F.data == "get_tasks")
 async def get_tasks(message: Message):
-    # await database.findOne("tasks")
     await message.answer(get_string("get_tasks_text"))
 
 
