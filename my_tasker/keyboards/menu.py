75c3c9078cdbd371542cd31c2ddc7054eb694255
Pavel <pavelvodocnikov@gmail.com>
2025-03-09 11:31:54 +0500

    рефакторинг menu.py

--- my_tasker/keyboards/menu.py
+++ my_tasker/keyboards/menu.py
@@ -1,16 +1,10 @@
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+button_1 = InlineKeyboardButton(text="Добавить задачу", callback_data="add_task")
+button_2 = InlineKeyboardButton(text="Список задач", callback_data="get_tasks")
+button_3 = InlineKeyboardButton(text="Удалить задачу", callback_data="delete_task")
+button_4 = InlineKeyboardButton(text="Получить задачу", callback_data="get_task")
+
 menu_keyboard = InlineKeyboardBuilder()
-menu_keyboard.add(
-    InlineKeyboardButton(text="Добавить задачу", callback_data="add_task")
-)
-menu_keyboard.add(
-    InlineKeyboardButton(text="Получить задачу", callback_data="get_task")
-)
-menu_keyboard.add(
-    InlineKeyboardButton(text="Удалить задачу", callback_data="delete_task")
-)
-menu_keyboard.add(
-    InlineKeyboardButton(text="Получить список задач", callback_data="get_tasks")
-)
+menu_keyboard.add(button_1, button_2, button_3)
